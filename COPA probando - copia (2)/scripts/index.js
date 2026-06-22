646cfc4d207d863c7663a21162a2a7898bb29bcc
brunoAlva1301 <alvarezbruno1301@gmail.com>
2026-06-22 20:28:56 -0300

    Delete COPA probando - copia (2) directory

--- COPA probando - copia (2)/scripts/index.js
+++
@@ -1,7 +0,0 @@
-function handleLogin() {
-    window.location.href = 'pages/league-selection.html';
-}
-
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter') handleLogin();
-});
